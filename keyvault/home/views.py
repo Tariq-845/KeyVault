77dbce72952a040234d5c5ad3d02a6c3c2e19ca6
Tariq Ebden <tebden16@gmail.com>
2024-07-09 17:39:52 +0100

    add: home template view

--- keyvault/home/views.py
+++ keyvault/home/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-def index(request):
-    return HttpResponse("PLEASE WORK!!!!")
+# def index(request):
+#     return HttpResponse("PLEASE WORK!!!!")
+
+class Home(TemplateView):
+    """
+    Template view to display the home page
+    (index.html)
+    """
+
+    template_name = "home/index.html"
--- keyvault/keyvault/urls.py
+++ keyvault/keyvault/urls.py
@@ -17,8 +17,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from home.views import Home
 
 urlpatterns = [
-    path("home/", include("home.urls")),
+    path("", Home.as_view(), name="home"),
     path("admin/", admin.site.urls),
 ]
